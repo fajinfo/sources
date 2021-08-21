8e871e3af67893cfbf76a7d63b264c48763b7264
swissfire <info@swiss-firefighters.ch>
2021-08-21 18:54:07 +0200

    Add user in dashboard

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,10 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
-            ->add('password')
+            ->add('roles', ChoiceType::class, array('choices' => [
+                'Utilisateur' => 'ROLE_USER',
+                'Administrateur' => 'ROLE_ADMIN'
+            ], 'expanded' => true, 'multiple' => true))
             ->add('sourcesAdmin')
             ->add('sourcesView')
         ;
