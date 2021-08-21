ca45836c81f96d2797ce3e31432907344ce45eca
swissfire <info@swiss-firefighters.ch>
2021-08-21 19:05:53 +0200

    Add user in dashboard

--- src/Controller/UserCrudController.php
+++ src/Controller/UserCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +15,16 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('username'),
+            ChoiceField::new('roles')->setChoices([
+                'Utilisateur' => 'ROLE_USER',
+                'Administrateur' => 'ROLE_ADMIN'
+            ])->setFormTypeOptions(['multiple' => true, 'expanded' => true]),
         ];
     }
-    */
 }
