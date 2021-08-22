cc7b61f21aae7cabb006ea9bc771c4780b246e13
swissfire <info@swiss-firefighters.ch>
2021-08-22 14:40:08 +0200

    Add toString to all entity

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('dashboard/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
